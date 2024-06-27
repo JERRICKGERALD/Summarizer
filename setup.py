df9d5a58ccc2166434749f53ab3225f341ae48ed
JERRICKGERALD <jerrick1105@gmail.com>
2024-06-27 14:24:17 -0400

    setup-uodated

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def get_requirements(file_path):
 setup(
     name='mlproject',
     version='0.0.1',
-    author='Jerrick Gerald'
+    author='Jerrick',
     author_email='jerrick1105@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
